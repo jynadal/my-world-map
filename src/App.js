e6d2e725ebae0b037e5bce5f2dd41f0919dbb4e1
jynadal <nadaljeanyves@hotmail.fr>
2020-06-16 14:07:35 +0200

    First commit with Map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// import Video from "./Video";
+import GeoChart from "./GeoChart";
+import data from "./GeoChart.world.geo.json";
+import "./App.css";
 
 function App() {
+  const [property, setProperty] = useState("pop_est");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <h2>World Map with d3-geo</h2>
+      <GeoChart data={data} property={property} />
+      <h2>Select property to highlight</h2>
+      <select
+        value={property}
+        onChange={event => setProperty(event.target.value)}
+      >
+        <option value="pop_est">Population</option>
+        <option value="name_len">Name length</option>
+        <option value="gdp_md_est">GDP</option>
+      </select>
+      {/* <Video /> */}
+    </React.Fragment>
   );
 }
 
